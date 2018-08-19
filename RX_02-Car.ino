1d5adea7c1ff4d8dc69d1cc50f591a18e12376b2
JotellyBarros <jotelly@gmail.com>
2018-08-19 18:25:58 -0300

    Add servo and beep

--- RX_02-Car.ino
+++ RX_02-Car.ino
@@ -1,4 +1,12 @@
-//Projeto RX_02 10/07/2018
+//Projeto RX_02 19/08/2018
+
+#include <Servo.h> //Biblioteca utilizada
+Servo servoMotor;  // Cria um Objeto servo chamado Servo
+
+int posServo = 120; //Definimos a variável que indica a posição do servo inicial
+int flagServo = 0;
+boolean setServoMotor = false;
+
 //Motor Esquerdo Dianteiro
 int motEDF = 4;
 int motEDT = 5;
@@ -15,15 +23,15 @@ int motDDT = 9;
 int motDTF = 10;
 int motDTT = 11;
 
+//Iluminacao
+int leds = 22;
+
 //Sesor Fotoeletrico
 int IRDireito = 52; // Escolhe pino de entrada para Sensor Infravermelho Direito
 int IREsquerdo = 53; // Escolhe pino de entrada para Sensor Infravermelho Esquerdo
 
-// variavel para leitura de status
-int valorStatus = 0;
-
 //Declaracao de velocidades
-int duration, distance, velocidade, velocidadeRe, velocidadeRot;
+int duration, distance, velocidade, velocidadeRot;
 int flag_motor_active = 0;
 
 //Define valor de curva
@@ -35,8 +43,33 @@ const int buzzer = 24;
 void setup() {
   velocidade = 0; //Define a velocidade dos motores frente
   velocidadeRot = 0; //Define a velocidade da Rotacao
-  velocidadeRe = 0; //Define a velocidade dos motores Re
+
+  pinMode(leds, OUTPUT);
+  digitalWrite(leds, HIGH); //Inicia com os Leds apagados
   Serial.begin(9600);
+  servoMotor.attach(50); //porta do Servo
+  servoMotor.write(posServo); //Comeca o motor na posicao Meio
+}
+
+void rotacaoServoMotor(boolean active) {
+  //flagServo 1 Esquerda
+  //flagServo 0 Meio
+  //flagServo 2 Direita
+
+  if (active == true) {
+    //Define a flag de apoio
+    if (posServo == 120) flagServo = 1;
+    else if (posServo == 180) flagServo = 2;
+
+    //Configura o incremento ou decremento do valor do Servo-Motor
+    if (flagServo == 1) posServo++;
+    else if (flagServo == 2) posServo--;
+
+  } else {
+    posServo = 150;
+  }
+
+  servoMotor.write(posServo);
 }
 
 void motorPare() {
@@ -44,6 +77,7 @@ void motorPare() {
   digitalWrite(motDDF, LOW);
   digitalWrite(motETF, LOW);
   digitalWrite(motEDF, LOW);
+
   digitalWrite(motDTT, LOW);
   digitalWrite(motDDT, LOW);
   digitalWrite(motETT, LOW);
@@ -56,26 +90,14 @@ void motorFrente() {
   analogWrite(motDDF, velocidade);
   analogWrite(motETF, velocidade);
   analogWrite(motEDF, velocidade);
-
-  //Parar tracao trazeira
-  digitalWrite(motDTT, LOW);
-  digitalWrite(motDDT, LOW);
-  digitalWrite(motETT, LOW);
-  digitalWrite(motEDT, LOW);
 }
 
 void motorRe() {
   motorPare();
-  analogWrite(motDTT, velocidadeRe);
-  analogWrite(motDDT, velocidadeRe);
-  analogWrite(motETT, velocidadeRe);
-  analogWrite(motEDT, velocidadeRe);
-
-  //Parar tracao Dianteira
-  digitalWrite(motDTF, LOW);
-  digitalWrite(motDDF, LOW);
-  digitalWrite(motETF, LOW);
-  digitalWrite(motEDF, LOW);
+  analogWrite(motDTT, velocidade);
+  analogWrite(motDDT, velocidade);
+  analogWrite(motETT, velocidade);
+  analogWrite(motEDT, velocidade);
 }
 
 void motorEsquerda() {
@@ -96,8 +118,8 @@ void motorDireita() {
 
 void motorFrente_CurvaEsquerda() {
   motorPare();
-  curva = ((velocidade/100) * 65);
-  
+  curva = ((velocidade / 100) * 65);
+
   analogWrite(motDTF, velocidade);
   analogWrite(motDDF, velocidade);
   analogWrite(motETF, velocidade - curva);
@@ -106,8 +128,8 @@ void motorFrente_CurvaEsquerda() {
 
 void motorFrente_CurvaDireita() {
   motorPare();
-  curva = ((velocidade/100) * 65);
-  
+  curva = ((velocidade / 100) * 65);
+
   analogWrite(motDTF, velocidade - curva);
   analogWrite(motDDF, velocidade - curva);
   analogWrite(motETF, velocidade);
@@ -133,28 +155,20 @@ void gerenciaVelocidade() {
 }
 
 void sensorIRDireito() {
-  valorStatus = digitalRead(IRDireito);
-  if (valorStatus == LOW) {
-    //Serial.println("Sensor IRDireito Ativado");
-    tone(buzzer,1730,100);
+  if (digitalRead(IRDireito) == LOW) {
     velocidadeRot = 220; //Define a velocidade da Rotacao
     motorEsquerda();
     delay (210);
     motorPare();
-    delay(100);
   }
 }
 
 void sensorIREsquerdo() {
-  valorStatus = digitalRead(IREsquerdo);
-  if (valorStatus == LOW) {
-    //Serial.println("Sensor IREsquerdo Ativado");
-    tone(buzzer,1730,100);
+  if (digitalRead(IREsquerdo) == LOW) {
     velocidadeRot = 220; //Define a velocidade da Rotacao
     motorDireita();
     delay (210);
     motorPare();
-    delay(100);
   }
 }
 
@@ -162,6 +176,9 @@ void loop() {
   //Leitura de Sensores Infra-Vermelho
   sensorIRDireito();
   sensorIREsquerdo();
+
+  //Verifica a posicao do Servo-Motor
+  rotacaoServoMotor(setServoMotor);
 
   //Verifica as flags de velocidade
   gerenciaVelocidade();
@@ -173,83 +190,66 @@ void loop() {
     //Parametros de velocidade
     switch (letra) {
       case '0':
-        velocidade = 1; //Define a velocidade dos motores frente
+        velocidade = 1; //Define a velocidade dos motores
         velocidadeRot = 1; //Define a velocidade da Rotacao
-        velocidadeRe = 1; //Define a velocidade dos motores Re
         break;
       case '1':
         velocidade = 70;
         velocidadeRot = 90;
-        velocidadeRe = 70;
         break;
       case '2':
         velocidade = 140;
         velocidadeRot = 140;
-        velocidadeRe = 140;
         break;
       case '3':
         velocidade = 170;
         velocidadeRot = 170;
-        velocidadeRe = 170;
         break;
       case '4':
         velocidade = 190;
         velocidadeRot = 190;
-        velocidadeRe = 190;
         break;
       case '5':
         velocidade = 200;
         velocidadeRot = 200;
-        velocidadeRe = 200;
         break;
       case '6':
         velocidade = 220;
         velocidadeRot = 220;
-        velocidadeRe = 220;
         break;
       case '7':
         velocidade = 230;
         velocidadeRot = 230;
-        velocidadeRe = 230;
         break;
       case '8':
         velocidade = 240;
         velocidadeRot = 240;
-        velocidadeRe = 240;
         break;
       case '9':
         velocidade = 245;
         velocidadeRot = 245;
-        velocidadeRe = 245;
         break;
       case 'q':
         velocidade = 255;
         velocidadeRot = 255;
-        velocidadeRe = 255;
         break;
       case 'F':
         flag_motor_active = 1;
-        motorFrente();
         break;
       case 'B':
         flag_motor_active = 2;
-        motorRe();
         break;
       case 'L':
         flag_motor_active = 3;
-        motorEsquerda();
         break;
       case 'R':
         flag_motor_active = 4;
-        motorDireita();
         break;
       case 'G':
         flag_motor_active = 5;
-        motorFrente_CurvaEsquerda();
         break;
       case 'I':
         flag_motor_active = 6;
-        motorFrente_CurvaDireita();
         break;
       case 'H':
         //--------------Nao Habilitado-------------//
@@ -260,13 +260,12 @@ void loop() {
       case 'S':
         //Parada automatica
         flag_motor_active = 0;
-        motorPare();
         break;
       case 'W':
-        //---------------------------//
+        digitalWrite(leds, LOW);
         break;
       case 'w':
-        //---------------------------//
+        digitalWrite(leds, HIGH);
         break;
       case 'U':
         //---------------------------//
@@ -275,23 +274,23 @@ void loop() {
         //---------------------------//
         break;
       case 'V':
-        tone(buzzer,1730);
+        tone(buzzer, 1730);
         break;
-      case 'v':        
+      case 'v':
         noTone(buzzer);
         break;
       case 'X':
-        //---------------------------//
+        posServo = 120; //Inicia o ciclo
+        setServoMotor = true;
         break;
       case 'x':
-        //---------------------------//
+        setServoMotor = false;
         break;
       case 'D':
         //---------------------------//
         break;
       default:
-        //Serial.println("Opcao Invalida");
-        //motorPare();
+        Serial.println("Opcao Invalida");
         break;
     } // end: switch
   }
